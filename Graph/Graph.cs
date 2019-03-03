9901c3fb1701f693f74b6c3bb770d8c0ec684c6a
Ivan Korotkii <vana0pub@gmail.com>
2019-03-03 20:43:28 +0300

    Классичекий обход в глубину и его применние для сортировки графа. В графе нет циклов.

---
+++ Graph/Graph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.Sockets;
+
+namespace Graph
+{
+    
+    public class Graph <V, E> : IEnumerable<E>
+    {
+
+        private List<List<V>> data;
+
+        private List<E> nodes;
+
+        public Graph(List<List<V>> v, List<E> n)
+        {
+            data = v;
+            nodes = n;
+
+        }
+
+        
+        
+        
+        private void dfs(int n, List<bool> visited, List<E> answer)
+        {
+            visited[n] = true;
+            List<V> node = data[n];
+            for (int x = 0; x < node.Count; x++)
+            {
+                if (!visited[x])
+                {
+                    dfs(x, visited, answer);
+                }
+            }
+            answer.Add(nodes[n]);
+        }
+        
+        private IEnumerable<E> TopologicalSort()
+        {
+            List<bool> visited = new List<bool>();
+            List<E> answer = new List<E>();
+            for (int x = 0; x < nodes.Count; x++)
+            {
+                visited.Add(false);
+            }
+            for (int x = 0; x < data.Count; x++)
+            {
+                if (!visited[x])
+                {
+                    dfs(x, visited, answer);
+                }
+            }
+            answer.Reverse();
+            return answer;
+        }
+            
+        public IEnumerator<E> GetEnumerator()
+        {
+            return TopologicalSort().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ TopologicalSorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Graph;
+
+namespace TopologicalSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<bool>> l = new List<List<bool>>();
+            l.Add(new List<bool>() {false,false,false,false,false});
+            l.Add(new List<bool>() {true,false,false,false,false});
+            l.Add(new List<bool>() {false,true,false,false,false});
+            l.Add(new List<bool>() {false,false,true,false,false});
+            l.Add(new List<bool>() {false,false,false,true,false});
+            List<int> m = new List<int>() {5,2,3,4,5};
+            
+            Graph<bool, int> g = new Graph<bool, int>(l, m);
+                        
+            List<int> arr = (from t in g select t).ToList();
+            foreach(var x in arr)
+                Console.WriteLine(x);
+        }
+    }
+}
